2b052384603f9131f3783ba5c3d6d61966dca3a9
Jake Medley <jakemedley2014@gmail.com>
2022-04-14 16:58:48 -0500

    updated to finish the arrayList Practice from the exercise 3.7.2.

--- src/exercises/ArrayListPractice.java
+++ src/exercises/ArrayListPractice.java
@@ -4,25 +4,60 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArrayListPractice {
-//    public static void main(String[] args) {
-//        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-//        double newSum = 0;
-//        for(int i: nums){
-//            if(nums[i] % 2 == 0) {
-//                newSum += nums[i];
-//            }
-//            System.out.println(newSum);
-//        }
-//    }
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int total = 0;
 
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(6);
+        myList.add(7);
+        myList.add(8);
+        myList.add(9);
+        myList.add(10);
+
+        System.out.println(evenAdd(myList));
+        String textExample = "Write a static method to print out each word in a list that has exactly 5 letters.";
+        System.out.println(fiveLetterWords(textExample));
+
+    }
+    public static int evenAdd (ArrayList<Integer> arr) {
+        int total = 0;
         for (int integer : arr) {
             if (integer % 2 == 0) {
                 total += integer;
             }
         }
-        System.out.println(total);
+        return total;
+    }
+
+    public static ArrayList<String> fiveLetterWords(String word){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What length of word would you like to print?");
+        int number = input.nextInt();
+        input.close();
+
+        String[] splitArray = word.split(" ");
+        ArrayList<String> myList = new ArrayList<>();
+
+        for (String s : splitArray) {
+            if (s.length() == number) {
+                myList.add(s);
+            }
+        }
+        return myList;
     }
 }
+
+//        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        int total = 0;
+//
+//        for (int integer : arr) {
+//            if (integer % 2 == 0) {
+//                total += integer;
+//            }
+//        }
+//        System.out.println(total);
+//    }
