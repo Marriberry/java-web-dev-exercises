e4303908f72b5b205f2f47aad4ec7af4d54c5441
Jake Medley <jakemedley2014@gmail.com>
2022-04-18 17:20:50 -0500

    finished exercise for ch4 parts 1-4

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,13 @@
 package org.launchcode.java.demos.lsn3classes1;
 
 public class SchoolPractice {
+
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student myself = new Student("Jake", 123, 1, 4.0);
+        System.out.println(myself.studentInfo());
+        myself.setGpa(3.8);
+        System.out.println(myself.studentInfo());
+        System.out.println(myself.getName());
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -5,9 +5,70 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class Student {
 
+    private static int nextStudentId = 1;
     private String name;
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
+
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
+    }
+
+    public Student(String name) {
+        this(name, nextStudentId);
+        nextStudentId++;
+    }
+
+    public String studentInfo() {
+        return (this.name + " StudentID: " + this.studentId + " has a GPA of: " + this.gpa + " and has taken " + this.numberOfCredits + " credit hours.");
+    }
+
+    public void setNextStudentID(int nextStudentId) {
+        this.nextStudentId = nextStudentId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    private void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public int getNextStudentId(){
+        return nextStudentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
 
 }
